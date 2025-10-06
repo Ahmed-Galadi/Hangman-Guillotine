06bc922fd4e3c00d8aca2d219b78ac6e5a3e05b5
Ahmed-Galadi <galadiahmed99@gmail.com>
2025-10-06 01:41:08 +0000

    fixes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 #include "game.hpp"
+#include <iostream>
 
 int main() {
-	std::string input;
-	game g;
+    std::string input;
+    game g;
 
-	while (true) {
-		std::cout << g;
-		std::getline(std::cin, input);
-		if (input[0] == EOF)
-			break;
-		g.setDiscoveredChar(input[0]);
-	}
+    while (!g.isGameOver()) {
+        std::cout << g;
+        std::cout << "\nEnter a letter: ";
+        
+        if (!std::getline(std::cin, input))
+            break;
+        
+        if (input.empty())
+            continue;
+        
+        if (input.length() > 1) {
+            std::cout << "Please enter only one character!\n";
+            continue;
+        }
+        
+        g.setDiscoveredChar(input[0]);
+    }
+    
+    std::cout << g;
+    return 0;
 }
